e7df65656a5af7be813ab4fcedb4a72835497e2d
mixinspace <islammadkaur@gmail.com>
2020-11-10 13:29:25 -0600

    commit tree e5a09d97ed7de3643d711c71bf5116da1eb5683a

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 16, "M", "white", "crazy", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 16, "M", "white", "crazy", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 16, "M", "white", "crazy", 1);
+INSERT INTO bears VALUES (5, "Melissa", 16, "M", "white", "crazy", 1);
+INSERT INTO bears VALUES (6, "Grinch", 16, "M", "white", "crazy", 1);
+INSERT INTO bears VALUES (7, "Wendy", 16, "M", "white", "crazy", 1);
+INSERT INTO bears VALUES (8, null, 16, "M", "white", "crazy", 1)
